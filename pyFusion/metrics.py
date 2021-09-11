a2c98b00267ef437d0c684348695e96d1454d00b
Cristian <cristian.crispini@outlook.it>
2021-09-11 10:54:33 +0200

    xydeas_petrovic_metric implemented

--- pyFusion/metrics.py
+++ pyFusion/metrics.py
@@ -1,9 +1,21 @@
 import numpy as np
+from math import exp, pi
 from scipy.ndimage import gaussian_filter
-from cv2 import cartToPolar, Sobel, CV_32F, normalize, NORM_MINMAX, filter2D
+from cv2 import KAZE_DIFF_PM_G1, KAZE_DIFF_PM_G2, cartToPolar, Sobel, CV_32F
 
 EPS = np.finfo(float).eps
-
+# xydeas_petrovic parameters
+# The constants Γ, κ , σ  and Γα, κα, σα determine 
+# the  exact  shape  of  the  sigmoid  functions  used  to  form  the  edge  strength  and  
+# orientation  preservation  values,
+GAMMA1 = 1
+GAMMA2 = 1
+K1 = -10
+K2 = -20
+DELTA1 = 0.5
+DELTA2 = 0.75
+L = 1
+#--------------------------
 def mutual_information_2d( origImage, fusedImage, sigma=1, normalized=False):
     """
     Computes (normalized) mutual information between two 1D variate from a
@@ -125,8 +137,48 @@ def _strenght_n_orientation(image):
     alpha = np.arctan(s_y / ( s_x + EPS))
     return (g, alpha)
 
+def _perceptual_loss(gA, gF, alphaA, alphaF):
+    # If g o alpha are followed by an underscore are ment to be considered written in uppercase
+    # The relative strength and orientation values of g_AF(n,m) and alpha_AF(n,m) of an input 
+    # image A with respect to F are formed as:
+    if (gA > gF):
+        g_AF = gF / ( gA + EPS)
+    else:
+        g_AF = gA / (gF + EPS)
+    
+    alpha_AF = np.abs( np.abs(alphaA - alphaF) - pi/2) / (pi/2)
+
+    qG_AF = GAMMA1 / (1 + exp( K1 *(g_AF - DELTA1)))
+    qalpha_AF = GAMMA2 / (1 + exp( K2 *(alpha_AF - DELTA2) ))
+    # These are used to derive the edge strength and orientation preservation values
+    # QgAF(n,m)  and  QαAF(n,m)  model  perceptual  loss  of  information  in  F,  in  terms  of  
+    # how well the strength and orientation values of a pixel p(n,m) in A are 
+    # represented in the fused image. 
+    #
+    # Edge  information preservation values are then defined as
+    q_AF = qG_AF * qalpha_AF
+    # with  0  ≤  Q AF(n,m)  ≤  1 .  A  value  of  0  corresponds  to  the  complete  loss  of  edge  
+    # information, at location (n,m), as transferred from A into F. QAF(n,m)=1 indicates 
+    # “fusion” from A to F with no loss of information. 
+    return q_AF
+
 def xydeas_petrovic_metric(image1, image2, fusedImage):
-    # Strenght and orientation for all the images
+    # EDGE Strenght and orientation for each pixels of the input images
     gA, alphaA = _strenght_n_orientation(image1)
     gB, alphaB = _strenght_n_orientation(image2)
     gF, alphaF = _strenght_n_orientation(fusedImage)
+    
+    q_AF = _perceptual_loss(gA, gF, alphaA, alphaF)
+    q_BF = _perceptual_loss(gB, gF, alphaB, alphaF)
+    #
+    # In general edge preservation values which 
+    # correspond to pixels with high edge strength, should influence normalised  weighted  
+    # performance metric QP more than 
+    # those of relatively low edge strength. Thus, wA(n,m)=[gA(n,m)]^L and 
+    # wB(n,m)=[gB(n,m)]^L where L is a constant.
+    #
+    wA = np.linalg.matrix_power(gA, L)
+    wB = np.linalg.matrix_power(gB, L)
+    # normalised  weighted  performance metric QP
+    qP_ABF = (q_AF * wA + q_BF * wB) / (wA + wB)
+
