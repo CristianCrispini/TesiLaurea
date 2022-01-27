400ed1d9ba57dd090c50b39cef131c0a3806961d
Cristian <cristian.crispini@outlook.it>
2022-01-27 18:21:43 +0100

    ultime modifiche

--- pyFusion/colorConverter.py
+++ pyFusion/colorConverter.py
@@ -1,4 +1,4 @@
-from cv2 import COLOR_RGB2YCrCb, COLOR_YCrCb2RGB, TonemapDrago, cvtColor, imread
+from cv2 import COLOR_RGB2YCrCb, COLOR_YCrCb2RGB, COLOR_BGR2GRAY, cvtColor, imread
 from numpy import clip, float32, uint8 
 
 class ColorConverter:
@@ -10,25 +10,25 @@ class ColorConverter:
         self.YCbCr_images = [-1 for i in images_path]
         for image in images_path:
             self.input_images.append( imread(image) )
-        self._to_grayscale()
+        self.normalize_gray_or_luminance()
 
-    def _RGB_to_YCbCr(self, img_RGB):
+    def RGB_to_YCbCr(self, img_RGB):
         """
-        A private method which converts an RGB image to YCrCb format
+        converts an RGB image to YCrCb format
         """
         img_RGB = img_RGB.astype(float32) / 255.
         return cvtColor(img_RGB, COLOR_RGB2YCrCb)
 
-    def _YCbCr_to_RGB(self, img_YCbCr):
+    def YCbCr_to_RGB(self, img_YCbCr):
         """
-        A private method which converts a YCrCb image to RGB format
+        converts a YCrCb image to RGB format
         """
         img_YCbCr = img_YCbCr.astype(float32)
         return cvtColor(img_YCbCr, COLOR_YCrCb2RGB)
 
-    def _is_gray(self, img):
+    def is_gray(self, img):
         """
-        A private method which returns True if image is gray, otherwise False
+        returns True if image is gray, otherwise False
         """
         if len(img.shape) < 3:
             return True
@@ -39,13 +39,13 @@ class ColorConverter:
             return True
         return False
 
-    def _to_grayscale(self):
+    def normalize_gray_or_luminance(self):
         """
-            A private method to convert all input images to YCbCr format
+            convert all input images to YCbCr format
         """
         for idx, img in enumerate(self.input_images):
-            if not self._is_gray(img):
-                self.YCbCr_images[idx] = self._RGB_to_YCbCr(img)
+            if not self.is_gray(img):
+                self.YCbCr_images[idx] = self.RGB_to_YCbCr(img)
                 self.normalized_images[idx] = self.YCbCr_images[idx][:, :, 0]
             else:
                 self.normalized_images[idx] = img / 255.
@@ -53,13 +53,21 @@ class ColorConverter:
     def to_color(self, fused_img):
         # Reconstruct fused image given rgb input images
         for idx, img in enumerate(self.input_images):
-            if not self._is_gray(img):
+            if not self.is_gray(img):
                 self.YCbCr_images[idx][:, :, 0] = fused_img
-                fused_final = self._YCbCr_to_RGB(self.YCbCr_images[idx])
-                #
-                # Given an interval, values outside the interval are clipped to the interval edges.
-                # For example, if an interval of [0, 1] is specified, values smaller than 0 become 0, and values larger than 1 become 1.
-                #
-                fused_final = clip(fused_final, 0, 1)
-        
-        return (fused_final * 255).astype(uint8)
+                fused_final = self.YCbCr_to_RGB(self.YCbCr_images[idx])
+
+        return self.display_image_asint(fused_final)
+
+    def to_gray(self, img):
+    
+        gray = cvtColor(img, COLOR_BGR2GRAY)
+        return gray
+
+    def display_image_asint(self, img):
+        #
+        # Given an interval, values outside the interval are clipped to the interval edges.
+        # For example, if an interval of [0, 1] is specified, values smaller than 0 become 0, and values larger than 1 become 1.
+        #
+        img = clip(img, 0, 1)
+        return (img * 255).astype(uint8)
--- pyFusion/iqa/metrics.py
+++ pyFusion/iqa/metrics.py
@@ -1,8 +1,11 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
+from scipy.stats import entropy
 
 from skimage.metrics import structural_similarity
 from skimage.metrics import mean_squared_error
+
+import matplotlib.pyplot as plt
 
 EPS = np.finfo(float).eps
 #--------------------------
@@ -44,6 +47,7 @@ def mutual_information_2d( origImage, fusedImage, sigma=1, normalized=False):
     jh = jh + EPS
     sh = np.sum(jh)
     jh = jh / sh
+
     s1 = np.sum(jh, axis=0).reshape((-1, jh.shape[0]))
     s2 = np.sum(jh, axis=1).reshape((jh.shape[1], -1))
 
@@ -60,7 +64,30 @@ def mutual_information_2d( origImage, fusedImage, sigma=1, normalized=False):
 
     return mi
 
-def entropy_2d(origImage,fusedImage):
+def kvalseth_MI(origImage,fusedImage):
+    """
+    Computes Mutual information (MI) In terms of entropies,
+    and using Kvalseth normalisation.
+    Parameters
+    ----------
+    origImage : 2D array
+        first variable
+    fusedImage : 2D array
+        second variable
+    Returns
+    -------
+    nmi: float
+        the computed similariy measure
+    """
+
+    information = entropy_2d(fusedImage, origImage)
+    Hx = entropy_2d(fusedImage,fusedImage)
+    Hy = entropy_2d(origImage,origImage)
+
+    return 2*(Hx + Hy - information) / (Hx + Hy)
+
+
+def entropy_2d(origImage,fusedImage, smooth=False):
     """
     Computes entropy between two 1D variate from a
     joint histogram.
@@ -70,6 +97,9 @@ def entropy_2d(origImage,fusedImage):
         first variable
     fusedImage : 1D array
         second variable
+    smooth: float
+        sigma for Gaussian smoothing of the joint histogram
+        default False
     Returns
     -------
     entropy: float
@@ -80,17 +110,15 @@ def entropy_2d(origImage,fusedImage):
 
     jh = np.histogram2d(origImage.ravel(), fusedImage.ravel(), bins=bins)[0]
 
+    # smooth the jh with a gaussian filter of given sigma
+    if smooth:
+        gaussian_filter(jh, sigma=1, mode='constant', output=jh)
+
     # compute marginal histograms
     jh = jh + EPS
     
+    # https://en.wikipedia.org/wiki/Joint_entropy
     sh = np.sum(jh)
     pi = jh / sh
-    entropy = -1 * np.sum(pi * np.log(pi))
+    entropy = -1 * np.sum(pi * np.log2(pi))
     return entropy
-
-def discrepancy(img1, img2):
-    P, Q = img1.shape
-    diff = img1 - img2
-    d = np.sum(np.absolute(diff)) / P * Q
-
-    return d
